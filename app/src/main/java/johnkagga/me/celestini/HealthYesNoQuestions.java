5d340abd3758b6f7ea5bc3e2e491f00e1c815350
johnkagga <johnkagga@gmail.com>
2016-01-06 09:34:32 +0300

    Added client and health info activities

--- app/src/main/java/johnkagga/me/celestini/HealthYesNoQuestions.java
+++ app/src/main/java/johnkagga/me/celestini/HealthYesNoQuestions.java
@@ -2,6 +2,7 @@ package johnkagga.me.celestini;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.UUID;
 
@@ -28,4 +29,69 @@ public class HealthYesNoQuestions extends ParseObject {
         return getBoolean("isSynced");
     }
 
+    public void setHistoryHypertension(String historyHypertension) {
+        put("HypertensionPP", historyHypertension);
+    }
+
+    public boolean getHypertensionHistory() {
+        return getBoolean("HypertensionPP");
+    }
+
+    public void setCesarean(String cesarean) {
+        put("Cesarean", cesarean);
+    }
+
+    public String getCesarean() {
+        return getString("Cesarean");
+    }
+
+    public void setHypertensive(String hypertensive) {
+        put("Hypertensive", hypertensive);
+    }
+
+    public String getHypertensive() {
+        return getString("Hypertensive");
+    }
+
+    public void setDiabetic(String diabetic) {
+        put("Diabetic", diabetic);
+    }
+
+    public String getDiabetic() {
+        return getString("Diabetic");
+    }
+
+    public void setChronicRenal(String chronic) {
+        put("ChronicRenal", chronic);
+    }
+
+    public String getChronicRenal() {
+        return getString("ChronicRenal");
+    }
+
+    public void setThyroid(String thyroid) {
+        put("Thyroid", thyroid);
+    }
+
+    public String getThyroid() {
+        return getString("Thyroid");
+    }
+
+    public void setSickleCell(String sickleCell) {
+        put("SickleCell", sickleCell);
+    }
+
+    public String getSickleCell() {
+        return getString("SickleCell");
+    }
+
+    public void setOwner(ClientContactInformation contactInformation)
+    {
+        put("InfoOwner",contactInformation);
+    }
+
+    public static ParseQuery<HealthYesNoQuestions> getQuery()
+    {
+        return ParseQuery.getQuery(HealthYesNoQuestions.class);
+    }
 }
