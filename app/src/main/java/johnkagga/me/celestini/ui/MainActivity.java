a61303d295ce3e8e09b41928b5d899055935f0a4
johnkagga <johnkagga@gmail.com>
2016-02-02 23:55:58 +0300

    working

--- app/src/main/java/johnkagga/me/celestini/ui/MainActivity.java
+++ app/src/main/java/johnkagga/me/celestini/ui/MainActivity.java
@@ -12,18 +12,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
-
-import java.util.List;
 
 import johnkagga.me.celestini.R;
-import johnkagga.me.celestini.data.ClientContactInformation;
-import johnkagga.me.celestini.utilites.Constants;
-import johnkagga.me.celestini.utilites.Helper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     private Button mInitialVisit;
     protected Button mSubVisit;
     private Button mTasks;
-    private Button mSync;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,14 +56,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mSync = (Button) findViewById(R.id.sync);
-        mSync.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SyncActivity.class);
-                startActivity(intent);
-            }
-        });
 
         currentUser = ParseUser.getCurrentUser();
 
@@ -137,55 +119,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Sync the data to Parse.com
-     */
-    private void syncData() {
-        if (Helper.isOnline(this)) {
-            if (ParseUser.getCurrentUser() != null) {
-                //If we have a user then sync the data
-                //Get the data from the LocalDataStore
-                ParseQuery<ClientContactInformation> query = ClientContactInformation.getQuery();
-                query.fromPin(Constants.INFO_SAVE_LABEL);
-                query.whereEqualTo(Constants.IS_SYNCED, false);
-                query.findInBackground(new FindCallback<ClientContactInformation>() {
-                    @Override
-                    public void done(List<ClientContactInformation> data, ParseException e) {
-                        if (e == null) {
-                            for (final ClientContactInformation info : data) {
-                                //set the sync status to true before syncing to parse
-                                info.setSync(true);
-                                info.saveInBackground(new SaveCallback() {
-                                    @Override
-                                    public void done(ParseException e) {
-                                        if (e == null) {
-                                            //Saving is successful
-                                            //Unpin from the local data store
-                                            info.unpinInBackground(Constants.INFO_SAVE_LABEL);
-                                            Helper.makeToast(MainActivity.this, "Sync successful");
-                                        } else {
-                                            //if saving fails set sync to false
-                                            info.setSync(false);
-                                            Log.e(LOG_TAG, "Error saving in background" + e.getMessage());
-                                        }
-                                    }
-                                });
-                            }
-                        } else {
-                            Helper.makeToast(MainActivity.this, "Error syncing: " + e.getMessage());
-                        }
-                    }
-                });
-            } else {
-                //Redirect to the login activity
-                Intent loginIntent = new Intent(this, LoginActivity.class);
-                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(loginIntent);
-            }
-        } else {
-            //No network connection
-            Helper.makeToast(this, "No internet connection");
-        }
-    }
+
 }
